4ab5893b4ec32c5b4c7d01782e2f7d247a9072f7
sdrobyshevsky <sdrobyshevsky@mail.ru>
2023-10-29 00:59:49 +0300

    Added info about task

--- UserDataFileWriterImpl.java
+++ UserDataFileWriterImpl.java
@@ -8,20 +8,21 @@ public class UserDataFileWriterImpl implements UserDataFileWriter {
 
   @Override
   public void writeUserData(UserData userData) throws IOException {
-    Scanner scanner = new Scanner(System.in);
-    System.out.println("Введите имя файла для записи данных: ");
-    String fileName = scanner.nextLine();
-    FileWriter writer = new FileWriter(fileName, true);
-    String userDataString = String.join(" ",
-        userData.getLastName(),
-        userData.getFirstName(),
-        userData.getMiddleName(),
-        userData.getDateOfBirth().format(DateTimeFormatter.ofPattern("dd.MM.yyyy")),
-        String.valueOf(userData.getPhoneNumber()),
-        userData.getGender().toString());
-    writer.write(userDataString + "\n");
-    writer.close();
-    System.out.println("Данные успешно записаны в файл: " +
-        fileName);
+    try (Scanner scanner = new Scanner(System.in)) {
+        System.out.println("Введите имя файла для записи данных: ");
+        String fileName = scanner.nextLine();
+        FileWriter writer = new FileWriter(fileName, true);
+        String userDataString = String.join(" ",
+            userData.getLastName(),
+            userData.getFirstName(),
+            userData.getMiddleName(),
+            userData.getDateOfBirth().format(DateTimeFormatter.ofPattern("dd.MM.yyyy")),
+            String.valueOf(userData.getPhoneNumber()),
+            userData.getGender().toString());
+        writer.write(userDataString + "\n");
+        writer.close();
+        System.out.println("Данные успешно записаны в файл: " +
+            fileName);
+    }
   }
 }
